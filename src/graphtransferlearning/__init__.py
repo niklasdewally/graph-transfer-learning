532308352c651b1fffd346e8d354635567eb5d3d
Niklas Dewally <nd60@st-andrews.ac.uk>
2023-04-15 20:43:39 +0100

    Add forest_fire and BA synthetic models
    Squashed commit of the following:
    
    commit e8b31f7c828b3a6fb823cd197d2397ec068f05bd
    Author: Niklas Dewally <nd60@st-andrews.ac.uk>
    Date:   Fri Apr 14 21:43:18 2023 +0100
    
        Allow use of existing labels in add_structural_labels
    
    commit 8bfa7dc019bdda64c4254cf42cdeaa0927378fcc
    Author: Niklas Dewally <nd60@st-andrews.ac.uk>
    Date:   Fri Apr 14 21:35:39 2023 +0100
    
        Add Barabasi-Albert graphs
    
    commit 01dbbb6b69fbc9c53d999189500a0184177ec290
    Author: Niklas Dewally <nd60@st-andrews.ac.uk>
    Date:   Fri Apr 14 21:25:57 2023 +0100
    
        Add structural labels to graphs
    
    commit 9052312539a52741dff378fb950132cd802b7df2
    Author: Niklas Dewally <nd60@st-andrews.ac.uk>
    Date:   Fri Apr 14 11:11:52 2023 +0100
    
        Bugfix
    
    commit 57c8f4eb4c8f94756a9a1157e2e12b6b512a49ec
    Author: Niklas Dewally <niklas@dewally.com>
    Date:   Thu Apr 13 14:45:27 2023 +0100
    
        Forest fire

--- src/graphtransferlearning/__init__.py
+++ src/graphtransferlearning/__init__.py
@@ -2,5 +2,5 @@ import graphtransferlearning.dataset
 import graphtransferlearning.features
 import graphtransferlearning.visualisations
 import graphtransferlearning.training
-
+from .graph_generators import generate_forest_fire, generate_barbasi, add_structural_labels
 
