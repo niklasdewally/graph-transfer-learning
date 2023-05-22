bcd15bddbc230348baf4cb20f85906835f0b2ddc
Niklas Dewally <niklas@dewally.com>
2023-05-22 23:23:13 +0100

    readd models folder to git

---
+++ src/gtl/models/__init__.py
@@ -0,0 +1,4 @@
+# ruff: noqa: F401
+from .egi import EGI
+from .classifier import LogisticRegression
+
---
+++ src/gtl/models/classifier.py
@@ -0,0 +1,25 @@
+"""
+This file contains classifiers for use with a separately trained unsupervised
+graph encoder (e.g. DGI, EGI, Node2Vec,...).
+"""
+
+
+import torch.nn as nn
+
+
+class LogisticRegression(nn.Module):
+    """
+    A logistic regression classifier.
+
+    Args:
+        input_dim: The dimension of the input.
+        n_classes: The number of classes. This forms the output dimension of
+                   the classifier.
+    """
+
+    def __init__(self, input_dim, n_classes):
+        super(LogisticRegression, self).__init__()
+        self.linear = nn.Linear(input_dim, n_classes)
+
+    def forward(self, x):
+        return self.linear(x)
